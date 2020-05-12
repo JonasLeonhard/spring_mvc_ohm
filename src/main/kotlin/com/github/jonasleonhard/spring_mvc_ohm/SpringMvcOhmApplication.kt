35070faf70846987e65b283a28beb66478955b09
JonasLeonhard <jonasleonhardfl@gmail.com>
2020-05-12 18:41:11 +0200

    feat(src): initialized project
    Initialized spring boot with kotlin and thymeleaf.
    
    References #Setup

---
+++ src/main/kotlin/com/github/jonasleonhard/spring_mvc_ohm/SpringMvcOhmApplication.kt
@@ -0,0 +1,11 @@
+package com.github.jonasleonhard.spring_mvc_ohm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcOhmApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvcOhmApplication>(*args)
+}
