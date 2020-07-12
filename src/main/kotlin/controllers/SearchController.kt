4888f4fe4cd993262b57bc7198560aa50a5bd80d
JonasLeonhard <jonasleonhardfl@gmail.com>
2020-07-12 18:49:23 +0200

    feat(recipe): added sums & likes
    - added escaped summaries to search results
    - added display of likes
    
    References #Search, #Recipe

--- src/main/kotlin/controllers/SearchController.kt
+++ src/main/kotlin/controllers/SearchController.kt
@@ -6,12 +6,15 @@ import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import services.JsoupService
 import services.RecipeService
 import services.UserService
 
 @Controller
 @RequestMapping("/search")
-class SearchController(val userService: UserService, val recipeService: RecipeService) {
+class SearchController(val userService: UserService,
+                       val recipeService: RecipeService,
+                       val jsoupService: JsoupService) {
 
     @GetMapping("")
     fun searchResults(model: Model, @RequestParam(value = "q") q: String, @RequestParam(value = "type") type: String): String {
@@ -39,6 +42,9 @@ class SearchController(val userService: UserService, val recipeService: RecipeSe
             val recipes = recipeService.search(q)
             println("${recipes.size} items: search got ... $recipes")
             model["recipes"] = recipes
+            model["escapedRecipeSummaries"] = recipes.map { recipe ->
+                jsoupService.escapeUserText(recipe.summary)
+            }
         }
     }
 }
