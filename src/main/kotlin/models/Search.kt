170cd43fbd7a57e81e7f1c4bfca743bc5df74b61
JonasLeonhard <jonasleonhardfl@gmail.com>
2020-11-19 21:34:32 +0100

    fix(search.kt): fix import missing
    - fixed a bug where import for temporal type was missing
    
    References #Fix

--- src/main/kotlin/models/Search.kt
+++ src/main/kotlin/models/Search.kt
@@ -3,10 +3,7 @@ package models
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.Instant
 import java.util.*
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Temporal
+import javax.persistence.*
 
 @Entity
 data class Search(
